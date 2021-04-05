dc9a2047af6aaabeaf91f562f3cde6d94f7a0d77
Vitalik-bog <78750613+Vitalik-bog@users.noreply.github.com>
2021-04-05 20:36:14 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,32 @@
 package com.company;
-        import java.util.Scanner;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.format("%.2f\n", Math.PI);
-        System.out.println("Imput a number");
-        double x = in.nextDouble();
-        double arg1 = 3 * Math.PI - 2 * (x);
-        double arg2 = 2 * Math.sin(arg1);
-        double arg3 = 5 * Math.PI + 2 * (x);
-        double arg4 = Math.pow (Math.cos(arg3),2);
-        double z = arg2 * arg4 ;
-        System.out.print(z);
-        in.close();
+        System.out.println("Введите размер массива");
+        int size = in.nextInt();
+        int array[] = new int[size];
+        System.out.println("Введите все элементы массива");
+        for (int i = 0; i < size; i++) {
+            array[i] = in.nextInt();
+        }
+        int min = array[0];
+        for (int i = 0; i < size; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+
+        }
+        System.out.println(min);
+
+        int max = array[size - 1];
+        for (int i = 0; i < size; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println(max);
     }
 }
