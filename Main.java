85a1834c2997870e838e3395a28c6b801a0a27a2
Vitalik-bog <78750613+Vitalik-bog@users.noreply.github.com>
2021-06-15 18:12:45 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,78 +1,34 @@
 package com.company;
-import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
-        Animal zoo = new Animal();
-        zoo.makeNoise();
-        Veterinar doctor = new Veterinar();
-        doctor.treatAnimal();
+        Person TOM=new Person();
+        TOM.fullname="Tom";
+        Person BOB=new Person("BOB",18);
+        TOM.move();
+        TOM.talk();
+        BOB.move();
+        BOB.talk();
     }
 }
-class Animal {
-
-    public void makeNoise() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Write food");
-        String food = in.nextLine();
-        System.out.println("Write location");
-        String location = in.nextLine();
-        System.out.print(food+"\n"+location);
-
+class Person {
+    String fullname;
+    int age;
+    Person()
+    {
+        this.fullname = fullname;
+        this.age = age;
     }
-    public void eat() {
-        System.out.println("ест");
-
+    Person (String f,int a)
+    {
+        this.fullname="BOB";
+        this.age=age;
     }
-    public void sleep(){
-        System.out.println("спит");
+    void move()
+    {
+        System.out.println(this.fullname + " speaking");
     }
-}
-class Dog extends Animal {
-    String Dog1 = "this.Dog1";
-    @Override
-    public void makeNoise() {
-        String Dog1 = new String("not sleep");
-    }
-    @Override
-    public void eat(){
-        String Dog1 = new String("eat");
-    }
-}
-class Cat extends Animal {
-    String Cat1 = "this.Cat1";
-
-    @Override
-    public void makeNoise() {
-        String Cat1 = new String("not sleep");
-
-    }
-
-    @Override
-    public void eat() {
-        String Cat1 = new String("eat");
-    }
-}
-class Horse extends Animal {
-    String Horse1 = "this.Horse1";
-
-
-    @Override
-    public void makeNoise() {
-        String Horse1 = new String("not sleep");
-
-    }
-
-    @Override
-    public void eat() {
-        String Horse1 = new String("eat");
-    }
-}
-class Veterinar{
-    public void treatAnimal(){
-        Animal cat1 = new Cat();
-
-
-
+    void talk()
+    {
+        System.out.println(this.fullname+ " not speaking");
     }
 }
