9b6cbaed4d42b793130be5ba005a7bf42fb25e5f
Vitalik-bog <78750613+Vitalik-bog@users.noreply.github.com>
2021-06-15 17:54:28 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,30 +2,77 @@ package com.company;
 import java.util.Scanner;
 public class Main {
 
-public static void main(String[] args) {
-Scanner in = new Scanner(System.in);
-System.out.println("Enter array length");
-int a = in.nextInt();
-int arrey[] = new int[a];
-System.out.println("Элемент массива");
-for (int b = 0; b < a; b++) {
-arrey[b] = in.nextInt();
+    public static void main(String[] args) {
+        Animal zoo = new Animal();
+        zoo.makeNoise();
+        Veterinar doctor = new Veterinar();
+        doctor.treatAnimal();
+    }
 }
-int min = arrey[0];
-for (int b = 0; b < a; b++) {
-if (arrey[b] < min) {
-min = arrey[b];
+class Animal {
+
+    public void makeNoise() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Write food");
+        String food = in.nextLine();
+        System.out.println("Write location");
+        String location = in.nextLine();
+        System.out.print(food+"\n"+location);
+
+    }
+    public void eat() {
+        System.out.println("ест");
+
+    }
+    public void sleep(){
+        System.out.println("спит");
+    }
 }
+class Dog extends Animal {
+    String Dog1 = "this.Dog1";
+    @Override
+    public void makeNoise() {
+        String Dog1 = new String("not sleep");
+    }
+    @Override
+    public void eat(){
+        String Dog1 = new String("eat");
+    }
 }
-int sum = 0;
-for (int b = 0; b < a; b++) {
-sum = sum + arrey[b];
+class Cat extends Animal {
+    String Cat1 = "this.Cat1";
+
+    @Override
+    public void makeNoise() {
+        String Cat1 = new String("not sleep");
+
+    }
+
+    @Override
+    public void eat() {
+        String Cat1 = new String("eat");
+    }
 }
-System.out.println(sum);
-double num = sum / a;
-double result = num * min;
-System.out.println(result);
-}
+class Horse extends Animal {
+    String Horse1 = "this.Horse1";
 
 
+    @Override
+    public void makeNoise() {
+        String Horse1 = new String("not sleep");
+
+    }
+
+    @Override
+    public void eat() {
+        String Horse1 = new String("eat");
+    }
+}
+class Veterinar{
+    public void treatAnimal(){
+        Animal cat1 = new Cat();
+
+
+
+    }
 }
